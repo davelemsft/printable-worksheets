d4c59e0b39e0c89bd8ca8e89e10972fbad09f13c
Aaron Powell <me@aaron-powell.com>
2020-07-07 04:44:39 +0000

    Adding custom colour selection

--- public/scripts/colours.js
+++ public/scripts/colours.js
@@ -7,23 +7,41 @@ const standardColours = [
   "orange",
   "black",
 ];
-
 const pallet = document.getElementById("pallet");
 const coloursList = document.getElementById("colours");
+
+const makeColour = (colour) => {
+  const item = document.createElement("li");
+  item.classList.add("colour");
+  item.style.backgroundColor = colour;
+  item.addEventListener("click", () => {
+    context.strokeStyle = colour;
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+  });
+  coloursList.appendChild(item);
+};
+
 const setupPallet = (context) => {
   pallet.addEventListener("click", togglePallet);
 
-  standardColours.map((colour) => {
-    const item = document.createElement("li");
-    item.classList.add("colour");
-    item.style.backgroundColor = colour;
-    item.addEventListener("click", () => {
-      context.strokeStyle = colour;
-      context.lineJoin = "round";
-      context.lineWidth = 5;
-    });
-    coloursList.appendChild(item);
+  standardColours.map(makeColour);
+
+  const input = document.createElement("input");
+  input.type = "color";
+  input.title = "Select a custom colour";
+  input.addEventListener("change", ({ target: { value } }) => {
+    makeColour(value);
+    context.strokeStyle = value;
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+    togglePallet();
   });
+
+  const item = document.createElement("li");
+  item.classList.add("colour");
+  item.appendChild(input);
+  coloursList.appendChild(item);
 
   context.strokeStyle = standardColours[0];
   context.lineJoin = "round";
