f3b1dcb27e317f158642fc9c79b05943e9f84450
Aaron Powell <me@aaron-powell.com>
2020-07-07 06:07:16 +0000

    adding all the conneect the dots images

--- public/scripts/connect-the-dots.js
+++ public/scripts/connect-the-dots.js
@@ -1,5 +1,9 @@
 const files = [
-  { label: "✏ Connecet the dots", path: "Connect the dots 1.svg" },
+  { label: "✏ Connecet the dots #1", path: "Connect the dots 1.svg" },
+  { label: "✏ Connecet the dots #2", path: "Connect the dots 2.svg" },
+  { label: "✏ Connecet the dots #3", path: "Connect the dots 3.svg" },
+  { label: "✏ Connecet the dots #4", path: "Connect the dots 4.svg" },
+  { label: "✏ Connecet the dots #5", path: "Connect the dots 5.svg" },
 ].map((f) => ({ ...f, path: `images/connect-the-dots/${f.path}` }));
 
 const loadFile = (file) => {
