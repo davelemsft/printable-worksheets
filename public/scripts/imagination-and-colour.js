38c0a96f3b6bef751f901d5c7ac6edd0bab51782
Aaron Powell <me@aaron-powell.com>
2020-07-29 00:56:54 +0000

    reducing what is available

--- public/scripts/imagination-and-colour.js
+++ public/scripts/imagination-and-colour.js
@@ -1,9 +1,9 @@
 const files = [
   { label: "🖌 Bit's Birthday", path: "Imagination and Colour 1.svg" },
-  { label: "🖌 Bit's Robot", path: "Imagination and Colour 2.svg" },
-  { label: "🖌 Bit's Castle", path: "Imagination and Colour 3.svg" },
-  { label: "🖌 Bit and Felix", path: "Imagination and Colour 4.svg" },
-  { label: "🖌 Bit's Favourite Season", path: "Imagination and Colour 5.svg" },
+  // { label: "🖌 Bit's Robot", path: "Imagination and Colour 2.svg" },
+  // { label: "🖌 Bit's Castle", path: "Imagination and Colour 3.svg" },
+  // { label: "🖌 Bit and Felix", path: "Imagination and Colour 4.svg" },
+  // { label: "🖌 Bit's Favourite Season", path: "Imagination and Colour 5.svg" },
 ].map((f) => ({ ...f, path: `images/imagination-and-colour/${f.path}` }));
 
 const loadFile = (file) => {
